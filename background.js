170a454cc997c08ec71327f1bf6cbfae4056b8db
RameshAditya <ramesh.aditya@gmail.com>
2018-06-16 00:03:06 +0530

    upload extension files

---
+++ background.js
@@ -0,0 +1,24 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
+chrome.alarms.onAlarm.addListener(function() {
+  chrome.browserAction.setBadgeText({text: ''});
+  chrome.notifications.create({
+      type:     'basic',
+      iconUrl:  'ducky.png',
+      title:    'Time\'s up!',
+      message:  'Quack!',
+      buttons: [
+        {title: 'You set an alarm to ring now.'}
+      ],
+      priority: 0});
+});
+
+chrome.notifications.onButtonClicked.addListener(function() {
+  chrome.storage.sync.get(['minutes'], function(item) {
+    chrome.browserAction.setBadgeText({text: 'ON'});
+    chrome.alarms.create({delayInMinutes: item.minutes});
+  });
+});
