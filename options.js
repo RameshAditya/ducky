ef7ed01c2027f64b7df71dfba50220f7fb594f5c
Tobin Rosenau <tobin.rosenau@mail.de>
2018-06-24 22:07:38 +0200

    added optionspage, possibility of adding four shortcuts

---
+++ options.js
@@ -0,0 +1,61 @@
+'use strict';
+// Saves options to chrome.storage
+function save_options() {
+    var searchResultTarget = document.getElementById('searchResultTarget').checked;
+    var firstUrl = document.getElementById('firstUrl').value;
+    var firstUrlIcon = document.getElementById('firstUrlIcon').value;
+    var secondUrl = document.getElementById('secondUrl').value;
+    var secondUrlIcon = document.getElementById('secondUrlIcon').value;
+    var thirdUrl = document.getElementById('thirdUrl').value;
+    var thirdUrlIcon = document.getElementById('thirdUrlIcon').value;
+    var fourthUrl = document.getElementById('fourthUrl').value;
+    var fourthUrlIcon = document.getElementById('fourthUrlIcon').value;
+    chrome.storage.sync.set({
+        searchResultTarget: searchResultTarget,
+        firstUrl: firstUrl,
+        firstUrlIcon: firstUrlIcon,
+        secondUrl: secondUrl,
+        secondUrlIcon: secondUrlIcon,
+        thirdUrl: thirdUrl,
+        thirdUrlIcon: thirdUrlIcon,
+        fourthUrl: fourthUrl,
+        fourthUrlIcon: fourthUrlIcon
+    }, function () {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function () {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    // Use default value color = 'red' and likesColor = true.
+    chrome.storage.sync.get({
+        searchResultTarget: true,
+        firstUrl: "",
+        firstUrlIcon: "",
+        secondUrl: "",
+        secondUrlIcon: "",
+        thirdUrl: "",
+        thirdUrlIcon: "",
+        fourthUrl: "",
+        fourthUrlIcon: ""
+    }, function (items) {
+        document.getElementById('searchResultTarget').checked = items.searchResultTarget;
+        document.getElementById('firstUrl').value = items.firstUrl;
+        document.getElementById('firstUrlIcon').value = items.firstUrlIcon;
+        document.getElementById('secondUrl').value = items.secondUrl;
+        document.getElementById('secondUrlIcon').value = items.secondUrlIcon;
+        document.getElementById('thirdUrl').value = items.thirdUrl;
+        document.getElementById('thirdUrlIcon').value = items.thirdUrlIcon;
+        document.getElementById('fourthUrl').value = items.fourthUrl;
+        document.getElementById('fourthUrlIcon').value = items.fourthUrlIcon;
+    });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
+document.getElementById('restore').addEventListener('click', restore_options);
--- popup.js
+++ popup.js
@@ -1,13 +1,49 @@
 'use strict';
 
 const quack = document.getElementById('quack');
-const duckTexts = ["Hello World", "Have you tried turning it off and on again?", "Could it have something to do with timezones?","What assumptions are you making?","Quack"];
+const duckTexts = ["Hello World", "Have you tried turning it off and on again?", "Could it have something to do with timezones?", "What assumptions are you making?", "Quack"];
+var searchResultTarget = true;
+var firstUrl = "";
+var firstUrlIcon = "";
+var secondUrl = "";
+var secondUrlIcon = "";
+var thirdUrl = "";
+var thirdUrlIcon = "";
+var fourthUrl = "";
+var fourthUrlIcon = "";
 
-function loadSettings() {
+async function loadSettings() {
     chrome.storage.sync.get({
-        soundEnabled: true
-    }, function(items) {
+        soundEnabled: true,
+        searchResultTarget: true,
+        firstUrl: "",
+        firstUrlIcon: "",
+        secondUrl: "",
+        secondUrlIcon: "",
+        thirdUrl: "",
+        thirdUrlIcon: "",
+        fourthUrl: "",
+        fourthUrlIcon: ""
+    }, async function (items) {
+        searchResultTarget = items.searchResultTarget;
         document.getElementById('sound').checked = items.soundEnabled;
+        searchResultTarget = items.searchResultTarget;
+        if (items.firstUrl !== "") {
+            document.getElementById('favicon1').setAttribute('src', items.firstUrlIcon);
+            document.getElementById('shortcut1').setAttribute('href', items.firstUrl);
+        }
+        if (items.secondUrl !== "") {
+            jQuery('#favicon2').attr('src', items.secondUrlIcon);
+            jQuery('#shortcut2').attr('href', items.secondUrl);
+        }
+        if (items.thirdUrl !== "") {
+            jQuery('#favicon3').attr('src', items.thirdUrlIcon);
+            jQuery('#shortcut3').attr('href', items.thirdUrl);
+        }
+        if (items.fourthUrl !== "") {
+            jQuery('#favicon4').attr('src', items.fourthUrlIcon);
+            jQuery('#shortcut4').attr('href', items.fourthUrl);
+        }
     });
 }
 
@@ -19,15 +55,23 @@ function playQuack() {
 function setAlarm(event) {
     let minutes = parseFloat(event.target.value);
 
-    chrome.browserAction.setBadgeText({text: 'ON'});
-    chrome.alarms.create({delayInMinutes: minutes});
-    chrome.storage.sync.set({minutes: minutes});
+    chrome.browserAction.setBadgeText({
+        text: 'ON'
+    });
+    chrome.alarms.create({
+        delayInMinutes: minutes
+    });
+    chrome.storage.sync.set({
+        minutes: minutes
+    });
 
     window.close();
 }
 
 function clearAlarm() {
-    chrome.browserAction.setBadgeText({text: ''});
+    chrome.browserAction.setBadgeText({
+        text: ''
+    });
     chrome.alarms.clearAll();
     window.close();
 }
@@ -38,8 +82,25 @@ function setSoundSetting(event) {
     });
 }
 
+function searchGoogle() {
+    if (searchResultTarget) {
+        window.location.href = "https://www.google.com/search?q=" + document.getElementById('searchTextInput').value.replace(' ', '+');
+    }else{
+        var win = window.open("https://www.google.com/search?q=" + document.getElementById('searchTextInput').value.replace(' ', '+'), '_blank');
+        win.focus();
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', loadSettings);
 document.getElementById('duckyImage').addEventListener('click', playQuack);
 document.getElementById('15min').addEventListener('click', setAlarm);
 document.getElementById('cancelAlarm').addEventListener('click', clearAlarm);
 document.getElementById('sound').addEventListener('change', setSoundSetting);
+document.getElementById('searchButton').addEventListener('click', searchGoogle);
+document.getElementById('searchTextInput').addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        searchGoogle();
+        return;
+    }
+}, false);
